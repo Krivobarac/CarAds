b11fabb02106545aaafa9c721013d297f4cde1a3
Saša Gašparović <33967593+Krivobarac@users.noreply.github.com>
2022-08-26 07:14:21 +0200

    Add localization (#11)
    * localization (uncomplete)
    
    * localization correction

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using CarAds.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using CarAds.Data;
+using Microsoft.AspNetCore.Mvc.Razor;
+using CarAds;
+using Microsoft.Extensions.Localization;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("IdentityCarAdsContextConnection") ?? throw new InvalidOperationException("Connection string 'IdentityCarAdsContextConnection' not found.");
@@ -11,8 +14,14 @@ var connectionString = builder.Configuration.GetConnectionString("IdentityCarAds
 builder.Services.AddDbContext<CarAdsContext>(option => option.UseSqlServer(CarAdsContext.GetConfiguration().GetConnectionString("PrimaryConnectionString")));
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>().AddEntityFrameworkStores<IdentityCarAdsContext>();
 builder.Services.AddDbContext<IdentityCarAdsContext>(option => option.UseSqlServer(IdentityCarAdsContext.GetConfiguration().GetConnectionString("IdentityCarAdsContextConnection")));
+
 builder.Services.AddAutoMapper(typeof(Program));
-builder.Services.AddControllersWithViews();
+
+builder.Services.AddLocalization(opts => opts.ResourcesPath = "Resources");
+builder.Services.AddControllersWithViews().
+    AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).
+        AddDataAnnotationsLocalization();
+
 builder.Services.AddTransient<CarManager>();
 builder.Services.AddTransient<ImageManager>();
 
@@ -35,14 +44,19 @@ carAdsContext.Database.Migrate();
 IdentityCarAdsContext identityCarAdsContext = scope.ServiceProvider.GetRequiredService<IdentityCarAdsContext>();
 identityCarAdsContext.Database.Migrate();
 
+// Localization options
+var supportedCultures = new[] { "en", "de-DE", "sr" };
+var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseStatusCodePagesWithReExecute("/Home/Error404/{0}");
-
 app.MapRazorPages();
 app.UseRouting();
 app.UseAuthentication();;
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
